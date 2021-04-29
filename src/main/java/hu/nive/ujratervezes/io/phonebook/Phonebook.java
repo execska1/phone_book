8f116adf7ca7e4296add10035d08e8d892a9e8c2
Sandor Gyapjas <execska@192.168.0.103>
2021-04-29 10:56:33 +0200

    Implement exportPhonebook method in Phonebook class

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,19 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class Phonebook {
+    public void exportPhonebook(Map<String, String> contacts, String output) throws IllegalArgumentException, IOException {
+        if (contacts == null || output == null) {
+            throw new IllegalArgumentException();
+        } else {
+            try(FileWriter fileWriter = new FileWriter(output)) {
+                for (Map.Entry<String, String> entry : contacts.entrySet()) {
+                    fileWriter.write(entry.getKey() + ": " + entry.getValue() + System.lineSeparator());
+                }
+            }
+        }
+    }
+}
